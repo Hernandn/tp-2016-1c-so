e4fa763aa3c63bb6dcc67667a8a7dcc51f53b6b4
andresjmichel <andresmichel13@gmail.com>
2016-04-17 18:17:29 -0300

    arme un socket servidor de prueba en el nucleo todavia no tengo bien claro como funca pero para que prueben

--- Nucleo/src/Nucleo.c
+++ Nucleo/src/Nucleo.c
@@ -7,12 +7,49 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <commons/log.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<sys/errno.h>
+
+
+int sockfd;
+struct sockaddr_in server;
+//static portnum = 0;
 
 int main(void) {
-	puts("Hola soy el Nucleo lalalala");
-	return EXIT_SUCCESS;
+
+	bzero(&server, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = 0;
+
+	// si no puede crear el socket pasa esto
+	if((sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	{
+		perror("fallo abriendo el socket \n");
+		exit(1);
+	}
+
+	// hago el blind del srv
+	if (bind(sockfd, (struct sockaddr*)&server, sizeof(server)))
+	{
+		perror("no se hizo el blind");
+		exit(1);
+	}
+
+	// activo modo escucha
+	if(listen(sockfd, 5) < 0)
+	{
+		//printf("no se pudo escuchar en el puerto %d : %s \n", portnum, sys_errlist[errno]);
+		exit(1);
+	}
+
+	close(sockfd);
 }
+
+
