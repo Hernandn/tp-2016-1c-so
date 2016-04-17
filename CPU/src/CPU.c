4b0157130f125b11b420f72dc705091aa58b1064
andresjmichel <andresmichel13@gmail.com>
2016-04-17 16:41:07 -0300

    agrego los proyectos de CPU y UMC con las commons configuradas

---
+++ CPU/src/CPU.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : CPU.c
+ Author      : Andres Michel
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("hola soy el cpu lalalal");
+	return EXIT_SUCCESS;
+}
---
+++ UMC/src/UMC.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : UMC.c
+ Author      : Andres Michel
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("hola soy el UMC");
+	return EXIT_SUCCESS;
+}
