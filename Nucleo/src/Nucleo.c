5ede57b9521b64f764f3d49c208ff730801b74c7
andresjmichel <andresmichel13@gmail.com>
2016-04-17 18:34:12 -0300

    arregle los warnings que tenia el codigo

--- Nucleo/src/Nucleo.c
+++ Nucleo/src/Nucleo.c
@@ -16,40 +16,25 @@
 #include<netdb.h>
 #include<sys/errno.h>
 
+int main(void)
+{
+	struct sockaddr_in direccionServidor;
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr = INADDR_ANY;
+	direccionServidor.sin_port = htons(8080);
 
-int sockfd;
-struct sockaddr_in server;
-//static portnum = 0;
+	int servidor = socket(AF_INET, SOCK_STREAM, 0);
 
-int main(void) {
-
-	bzero(&server, sizeof(server));
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = 0;
-
-	// si no puede crear el socket pasa esto
-	if((sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	if (bind(servidor, (void*) &direccionServidor, sizeof(direccionServidor)) != 0)
 	{
-		perror("fallo abriendo el socket \n");
-		exit(1);
+		perror("fallo el bind capo!!");
+		return 1;
 	}
+	printf("Estoy escuchando\n");
+	listen(servidor, 100);
 
-	// hago el blind del srv
-	if (bind(sockfd, (struct sockaddr*)&server, sizeof(server)))
-	{
-		perror("no se hizo el blind");
-		exit(1);
-	}
-
-	// activo modo escucha
-	if(listen(sockfd, 5) < 0)
-	{
-		//printf("no se pudo escuchar en el puerto %d : %s \n", portnum, sys_errlist[errno]);
-		exit(1);
-	}
-
-	close(sockfd);
+	for(;;);
+	return 0;
 }
 
 
