44c65586b88289d42ffb349396275f90e327e270
andresjmichel <andresmichel13@gmail.com>
2016-04-17 15:40:49 -0300

    acabo de crear el nucleo

---
+++ Nucleo/src/Nucleo.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Nucleo.c
+ Author      : Andres Michel
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hola soy el Nucleo lalalala");
+	return EXIT_SUCCESS;
+}
