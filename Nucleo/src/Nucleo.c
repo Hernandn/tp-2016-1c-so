3cdd05617fcd87e4898a5760dc42730bdbaa60e1
hernan <hernan.nogare@gmail.com>
2016-04-19 00:01:42 -0300

    probando levantar archivo de configuracion en el Nucleo

--- Nucleo/src/Nucleo.c
+++ Nucleo/src/Nucleo.c
@@ -10,14 +10,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <commons/log.h>
+#include <commons/config.h>
 #include<sys/socket.h>
 #include<sys/types.h>
 #include<netinet/in.h>
 #include<netdb.h>
 #include<sys/errno.h>
+#include "configuration.h"
 
 int main(void)
 {
+	//probando levantar archivo de configuracion
+	t_config* nConfig = config_create(NUCLEO_CONFIG_PATH);
+	printf("Archivo de configuracion: \n");
+	printf("%s: %d\n",PUERTO_PROG,config_get_int_value(nConfig,PUERTO_PROG));
+	printf("%s: %d\n",PUERTO_CPU,config_get_int_value(nConfig,PUERTO_CPU));
+	printf("%s: %d\n",QUANTUM,config_get_int_value(nConfig,QUANTUM));
+	printf("%s: %d\n",QUANTUM_SLEEP,config_get_int_value(nConfig,QUANTUM_SLEEP));
+
+	//hay que ver como sacar el length de este array de strings
+	char** array1 = config_get_array_value(nConfig,SEM_IDS);
+	int i;
+	for(i=0;i<3;i++){
+		printf("array[%d]: %s\n",i,array1[i]);
+	}
+	/*
+	char** array2 = config_get_array_value(nConfig,SEM_INIT);
+	char** array3 = config_get_array_value(nConfig,IO_IDS);
+	char** array4 = config_get_array_value(nConfig,IO_SLEEP);
+	char** array5 = config_get_array_value(nConfig,SHARED_VARS);
+	*/
+
+
+
+
+	//--------------
+
+	/******sockets*******/
 	struct sockaddr_in direccionServidor;
 	direccionServidor.sin_family = AF_INET;
 	direccionServidor.sin_addr.s_addr = INADDR_ANY;
---
+++ Nucleo/src/configuration.h
@@ -0,0 +1,22 @@
+/*
+ * config.h
+ *
+ *  Created on: 18/4/2016
+ *      Author: utnso
+ */
+
+#ifndef CONFIGURATION_H_
+#define CONFIGURATION_H_
+
+#define NUCLEO_CONFIG_PATH "nucleo.conf"
+#define PUERTO_PROG "PUERTO_PROG"
+#define PUERTO_CPU "PUERTO_CPU"
+#define QUANTUM "QUANTUM"
+#define QUANTUM_SLEEP "QUANTUM_SLEEP"
+#define SEM_IDS "SEM_IDS"
+#define SEM_INIT "SEM_INIT"
+#define IO_IDS "IO_IDS"
+#define IO_SLEEP "IO_SLEEP"
+#define SHARED_VARS "SHARED_VARS"
+
+#endif /* CONFIGURATION_H_ */
