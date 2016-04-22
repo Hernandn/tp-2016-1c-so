f99878b47bc9b0f441239b7f69d3ed0bae1980b3
Andres Martinez <andres93.lml@gmail.com>
2016-04-21 22:13:47 -0300

    Falto el archivo consola.h, mala mia.

---
+++ Consola/src/consola.h
@@ -0,0 +1,18 @@
+/*
+ * consola.h
+ *
+ *  Created on: 21/4/2016
+ *      Author: utnso
+ */
+
+#ifndef CONSOLA_H_
+#define CONSOLA_H_
+
+//codigos de operaciones de la UMC
+//esto tenemos que ver bien despues que decidimos hacer para codificarlas
+#define ANSISOP_PROGRAM 1
+#define HANDSHAKE 5
+//---------------------
+
+
+#endif /* CONSOLA_H_ */
